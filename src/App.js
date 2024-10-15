b76feb2c40cbb53cc88703474a5f7f9a96c0bd0f
Baruc Carrillo <baruccarrillo@outlook.com>
2024-10-14 20:10:19 -0700

    IMPLEMENTACIÓN DE API-FUTBOL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [match, setMatch] = useState(null);
+
+  useEffect(() => {
+    fetch("https://v3.football.api-sports.io/fixtures?live=all", {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "6c148b3d78a635913eba9886ba0b1d97",
+        "x-rapidapi-host": "v3.football.api-sports.io",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.response);
+      setMatch(data.response);
+    })
+    .catch((error) => console.log("error", error));
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +32,22 @@ function App() {
           <ul><a href='#'>TOPS</a></ul>
         </nav>
       </header>
+      <div id="wg-api-football-games"
+          data-host="v3.football.api-sports.io"
+          data-key="6c148b3d78a635913eba9886ba0b1d97"
+          data-date=""
+          data-league=""
+          data-season=""
+          data-theme=""
+          data-refresh="30"
+          data-show-toolbar="true"
+          data-show-errors="false"
+          data-show-logos="false"
+          data-modal-game="true"
+          data-modal-standings="true"
+          data-modal-show-logos="true"
+          class="match-live">
+      </div>
     </div>
   );
 }
